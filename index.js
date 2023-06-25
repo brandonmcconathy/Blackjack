eef132e6badf9359f28919236d8158724cd7b217
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-06-24 20:36:49 -0700

    Added dealers turn
    After the player stands it then goes to the dealers turn where the dealer will draw a card until they get to 17 or more. Then it compares the score of the dealer and the player to find the winner.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bustedEl = document.getElementById("busted-btn")
 const blackjackEl = document.getElementById("blackjack-btn")
 const winEl = document.getElementById("win-btn")
 const pushEl = document.getElementById("push-btn")
+const loseEl = document.getElementById("lose-btn")
 let deckId = ""
 let cards = new Array()
 let playerValue = 0
@@ -14,6 +15,7 @@ bustedEl.addEventListener("click", gameplay)
 blackjackEl.addEventListener("click", gameplay)
 winEl.addEventListener("click", gameplay)
 pushEl.addEventListener("click", gameplay)
+loseEl.addEventListener("click", gameplay)
 
 function gameplay() {
   resetmodal()
@@ -30,7 +32,6 @@ async function startGame() {
   }
   renderGameplay()
   playerValue = updateValue(playerValue, cards[0].value)
-  dealerValue = updateValue(dealerValue, cards[1].value)
   playerValue = updateValue(playerValue, cards[2].value)
   dealerValue = updateValue(dealerValue, cards[3].value)
   console.log(cards)
@@ -45,6 +46,7 @@ async function drawCard() {
 function renderGameplay() {
   containerEl.innerHTML = `
   <h1>Dealer</h1>
+  <h4 id="dealer-count">0</h4>
   <div class="card-container" id="dealer-cards">
     <img class="hidden" src="${cards[0].images.png}">
     <img class="hidden" src="${cards[0].images.png}">
@@ -53,7 +55,7 @@ function renderGameplay() {
     <img class="hidden" src="${cards[0].images.png}">
     <img class="hidden" src="${cards[0].images.png}">
   </div>
-  <h4 id="count">0</h4>
+  <h4 id="player-count">0</h4>
   <div class="action-btns">
     <button class="action-btn" id="hit" disabled>Hit</button>
     <button class="action-btn" id="stand" disabled>Stand</button>
@@ -71,7 +73,8 @@ function renderGameplay() {
         setTimeout(() => {dealerEl.innerHTML += `<img src="${cards[3].image}">`
         document.getElementById("hit").disabled = false
         document.getElementById("stand").disabled = false
-        document.getElementById("count").textContent = playerValue
+        document.getElementById("player-count").textContent = playerValue
+        document.getElementById("dealer-count").textContent = dealerValue
         if (playerValue === 21) {
           blackjack()
         }
@@ -81,7 +84,7 @@ function renderGameplay() {
   }, 700)
 
   document.getElementById("hit").addEventListener("click", hit)
-  document.getElementById("stand").addEventListener("click", hit)
+  document.getElementById("stand").addEventListener("click", dealersTurn)
 }
 
 function updateValue(value, card) {
@@ -110,13 +113,16 @@ async function hit() {
   card = await drawCard()
   document.getElementById("player-cards").innerHTML += `<img src="${card.image}">`
   playerValue = updateValue(playerValue, card.value)
-  document.getElementById("count").textContent = playerValue
+  document.getElementById("player-count").textContent = playerValue
   checkValue(playerValue)
 }
 
 function resetmodal() {
   document.getElementById("busted").style.display = "none"
   document.getElementById("blackjack").style.display = "none"
+  document.getElementById("win").style.display = "none"
+  document.getElementById("push").style.display = "none"
+  document.getElementById("lose").style.display = "none"
   document.getElementById("container").style.filter = ""
 }
 
@@ -126,6 +132,7 @@ function busted() {
   document.getElementById("busted").style.display = "block"
   document.getElementById("container").style.filter = "blur(2.5px)"
   playerValue = 0
+  dealerValue = 0
 }
 
 function blackjack() {
@@ -134,4 +141,67 @@ function blackjack() {
   document.getElementById("blackjack").style.display = "block"
   document.getElementById("container").style.filter = "blur(2.5px)"
   playerValue = 0
+  dealerValue = 0
 }
+
+function win() {
+  document.getElementById("hit").disabled = true
+  document.getElementById("stand").disabled = true
+  document.getElementById("win").style.display = "block"
+  document.getElementById("container").style.filter = "blur(2.5px)"
+  playerValue = 0
+  dealerValue = 0
+}
+
+function push() {
+  document.getElementById("hit").disabled = true
+  document.getElementById("stand").disabled = true
+  document.getElementById("push").style.display = "block"
+  document.getElementById("container").style.filter = "blur(2.5px)"
+  playerValue = 0
+  dealerValue = 0
+}
+
+function lose() {
+  document.getElementById("hit").disabled = true
+  document.getElementById("stand").disabled = true
+  document.getElementById("lose").style.display = "block"
+  document.getElementById("container").style.filter = "blur(2.5px)"
+  playerValue = 0
+  dealerValue = 0
+}
+
+async function dealersTurn() {
+  document.getElementById("hit").disabled = true
+  document.getElementById("stand").disabled = true
+  document.getElementById("dealer-cards").innerHTML = `
+  <img src="${cards[1].image}">
+  <img src="${cards[3].image}">
+  `
+  dealerValue = updateValue(dealerValue, cards[1].value)
+  document.getElementById("dealer-count").textContent = dealerValue
+
+  while (dealerValue < 18) {
+    card = await drawCard()
+    document.getElementById("dealer-cards").innerHTML += `<img src="${card.image}">`
+    dealerValue = await updateValue(dealerValue, card.value)
+    document.getElementById("dealer-count").textContent = dealerValue
+  }
+
+  if (dealerValue > 21) {
+    setTimeout(win, 1000)
+  } else {
+    setTimeout(compareCounts, 1000)
+  }
+}
+
+function compareCounts() {
+  if (dealerValue > playerValue) {
+    lose()
+  } else if (dealerValue < playerValue) {
+    win()
+  } else {
+    push()
+  }
+}
+

1bf617d0945cdfd9da8d33ff84703439d73eba7b
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-06-27 14:06:54 -0700

    Implemented betting system and fixed page layout
    Implemented a betting system that allows the user to place a bet before starting their hand. Changed the layout of the page to look better on iPhone by having more space on the bottom of the screen.

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-const startEl = document.getElementById("start-game")
 const containerEl = document.getElementById("container")
-const bustedEl = document.getElementById("busted-btn")
-const blackjackEl = document.getElementById("blackjack-btn")
-const winEl = document.getElementById("win-btn")
-const pushEl = document.getElementById("push-btn")
-const loseEl = document.getElementById("lose-btn")
+
 let deckId = ""
 let cards = new Array()
 let player = {
@@ -17,34 +12,30 @@ let dealer = {
   Aces: 0,
   value: 0
 }
+let chips = {
+  current: 100,
+  bet: 0,
+  currentBet: 0
+}
 
-startEl.addEventListener("click", gameplay)
-bustedEl.addEventListener("click", gameplay)
-blackjackEl.addEventListener("click", gameplay)
-winEl.addEventListener("click", gameplay)
-pushEl.addEventListener("click", gameplay)
-loseEl.addEventListener("click", gameplay)
-
-function gameplay() {
-  resetmodal()
-  startGame()
+for (let i = 0; i < 6; i++) {
+  document.getElementsByClassName("new-game")[i].addEventListener("click", renderBet)
 }
 
 async function startGame() {
-  containerEl.innerHTML = "<h1>loading...</h1>"
+  containerEl.innerHTML = `<h1 class="loading">loading...</h1>`
   const response = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6")
   const data = await response.json()
   deckId = data.deck_id
   for (let i=0; i < 4; i++){
     cards[i] = await drawCard()
   }
-  renderGameplay()
   player.cards.push(cards[0], cards[2])
   dealer.cards.push(cards[1], cards[3])
   player.value = updateValue(player.value, cards[0].value)
   player.value = updateValue(player.value, cards[2].value)
   dealer.value = updateValue(dealer.value, cards[3].value)
-  console.log(cards)
+  renderGameplay()
 }
 
 async function drawCard() {
@@ -53,10 +44,102 @@ async function drawCard() {
   return data.cards[0]
 }
 
+function renderBet() {
+  resetmodal()
+  containerEl.innerHTML = `
+  <h1>Place your bets</h1>
+  <h1 id="current-bet">0</h1>
+  <div class="bet-btns">
+    <button class="add-btn bet-btn" value="1">1</button>
+    <button class="add-btn bet-btn" value="5"">5</button>
+    <button class="add-btn bet-btn" value="10">10</button>
+    <button class="add-btn bet-btn" value="25">25</button>
+  </div>
+  <div class="bet-btns">
+    <button class="minus-btn bet-btn" value="-1" disabled>-1</button>
+    <button class="minus-btn bet-btn" value="-5" disabled>-5</button>
+    <button class="minus-btn bet-btn" value="-10" disabled>-10</button>
+    <button class="minus-btn bet-btn" value="-25" disabled>-25</button>
+  </div>
+  <button id="place-bet">Place Bet</button>
+  `
+  checkBalance()
+  for (let i = 0; i < 8; i++) {
+    document.getElementsByClassName("bet-btn")[i].addEventListener("click", updateBet)
+  }
+  document.getElementById("place-bet").addEventListener("click", placeBet)
+}
+
+function updateBet() {
+  chips.currentBet += Number(this.value)
+  chips.current -= Number(this.value)
+  checkBalance()
+  document.getElementById("current-bet").textContent = chips.currentBet
+}
+
+function placeBet() {
+  chips.bet = chips.currentBet
+  startGame()
+}
+
+function checkBalance() {
+  // Checks if the add buttons should be disabled or enabled
+  if (chips.current < 1) {
+    for (let i = 0; i < 4; i++) {
+      document.getElementsByClassName("add-btn")[i].disabled = true
+    }
+  } else if (chips.current < 5) {
+      document.getElementsByClassName("add-btn")[0].disabled = false
+      for (let i = 1; i < 4; i++) {
+        document.getElementsByClassName("add-btn")[i].disabled = true
+      }
+  } else if (chips.current < 10) {
+      for (let i = 2; i < 4; i++) {
+        document.getElementsByClassName("add-btn")[i - 2].disabled = false
+        document.getElementsByClassName("add-btn")[i].disabled = true
+      }
+  } else if (chips.current < 25) {
+      for (let i = 0; i < 3; i++) {
+        document.getElementsByClassName("add-btn")[i].disabled = false
+      }
+      document.getElementsByClassName("add-btn")[3].disabled = true
+  } else {
+      for (let i = 0; i < 4; i++) {
+        document.getElementsByClassName("add-btn")[i].disabled = false
+      }
+  }
+
+  // Checks if the minus buttons should be disabled or enabled
+  if (chips.currentBet < 1) {
+    for (let i = 0; i < 4; i++) {
+      document.getElementsByClassName("minus-btn")[i].disabled = true
+    }
+  } else if (chips.currentBet < 5) {
+      document.getElementsByClassName("minus-btn")[0].disabled = false
+      for (let i = 1; i < 4; i++) {
+        document.getElementsByClassName("minus-btn")[i].disabled = true
+      }
+  } else if (chips.currentBet < 10) {
+      for (let i = 2; i < 4; i++) {
+        document.getElementsByClassName("minus-btn")[i - 2].disabled = false
+        document.getElementsByClassName("minus-btn")[i].disabled = true
+      }
+  } else if (chips.currentBet < 25) {
+      for (let i = 0; i < 3; i++) {
+        document.getElementsByClassName("minus-btn")[i].disabled = false
+      }
+      document.getElementsByClassName("minus-btn")[3].disabled = true
+    } else {
+      for (let i = 0; i < 4; i++) {
+        document.getElementsByClassName("minus-btn")[i].disabled = false
+      }
+  }
+}
+
 function renderGameplay() {
   containerEl.innerHTML = `
-  <h1>Dealer</h1>
-  <h4 id="dealer-count">0</h4>
+  <p class="chips">Chips: <span id="current-chips">${chips.current}</span></p>
+  <h4 class="count" id="dealer-count">0</h4>
   <div class="card-container" id="dealer-cards">
     <img class="hidden" src="./images/card-back.png">
     <img class="hidden" src="${cards[3].images.png}">
@@ -65,13 +148,13 @@ function renderGameplay() {
     <img class="hidden" src="${cards[0].images.png}">
     <img class="hidden" src="${cards[2].images.png}">
   </div>
-  <h4 id="player-count">0</h4>
+  <h4 class="count" id="player-count">0</h4>
   <div class="action-btns">
     <button class="action-btn" id="hit" disabled>Hit</button>
     <button class="action-btn" id="double" disabled>Double</button>
     <button class="action-btn" id="stand" disabled>Stand</button>
   </div>
-  <h1>Player</h1>
+  <p>Bet: <span id="bet">${chips.bet}</span></p>
   `
 
   const playerEl = document.getElementById("player-cards")
